b4d909cc20c6cab3df150dac67b35f2ec45fe28c
Akash Devdhar <neospeed83@gmail.com>
2019-11-25 12:58:55 -0700

    Added set boundaries button and formatting

--- package360/src/LandingPage.java
+++ package360/src/LandingPage.java
@@ -16,29 +16,31 @@ public class LandingPage extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         //set boundaries
-        JLabel lowerBound = new JLabel("select low bound");
+        JLabel lowerBound = new JLabel("select lower bound");
         JTextField tfLower = new JTextField(50);
         lowerBound.setLabelFor(tfLower);
         setLayout(null);
         add(lowerBound);
         add(tfLower);
-        lowerBound.setBounds(90, 200, 100, 30);
-        tfLower.setBounds(200, 200, 100, 30);
+        lowerBound.setBounds(20, 100, 120, 30);
+        tfLower.setBounds(150, 100, 50, 30);
 
         JLabel upperBound = new JLabel("select upper bound");
         JTextField tfUpper = new JTextField(50);
         upperBound.setLabelFor(tfUpper);
         add(upperBound);
         add(tfUpper);
-        upperBound.setBounds(390, 200, 120, 30);
-        tfUpper.setBounds(530, 200, 100, 30);
+        upperBound.setBounds(320, 100, 120, 30);
+        tfUpper.setBounds(460, 100, 50, 30);
 
+        JButton setBoundaries = new JButton("Set");
+        setBoundaries.setBounds(670, 100, 60, 30);
+        add(setBoundaries);
 
         //browse button
         JButton browseButton = new JButton("load a file");
-        setLayout(null);
         add(browseButton);
-        browseButton.setBounds(20, 20, 100, 30);
+        browseButton.setBounds(20, 200, 100, 30);
         browseButton.addActionListener(e -> {
             JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
             jfc.setDialogTitle("Select a csv or txt");

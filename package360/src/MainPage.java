040983f4d555921562f929aa6f14337e293ae14e
Akash Devdhar <neospeed83@gmail.com>
2019-11-28 18:13:01 -0700

    Added update graph

--- package360/src/MainPage.java
+++ package360/src/MainPage.java
@@ -5,7 +5,6 @@ import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 import javax.swing.*;
@@ -19,23 +18,29 @@ import java.util.List;
 class MainPage extends JFrame {
 
     private File selectedFile;
+    private List<Integer> fileData;
+    private DefaultCategoryDataset dataset;
+    //Flags
     private static boolean boundaryFlag = true;
+    private static boolean updateFlag = false;
 
     MainPage(File inputFile) throws Exception {
         selectedFile = inputFile;
+        dataset = new DefaultCategoryDataset();
         InitMainUI();
     }
 
     private void InitMainUI() throws Exception {
         setLayout(null);
-        setTitle("Grade Analytics - Main Page :" + selectedFile.getName());
+        setTitle("Grade Analytics - Main Page : " + selectedFile.getName());
         setSize(1000, 600);
         setLocationRelativeTo(null);
         this.setExtendedState(this.getExtendedState() | JFrame.MAXIMIZED_BOTH);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        // Load file filedata into -> filedata
-        List<Integer> filedata = ReadFile.readFileByName(selectedFile.getPath());
-        for (Integer i : filedata) {
+
+        // Load file fileData into -> fileData
+        fileData = ReadFile.readFileByName(selectedFile.getPath());
+        for (Integer i : fileData) {
             int higher = i.compareTo(SetBoundary.getHigherBound());
             int lower = i.compareTo(SetBoundary.getLowerBound());
             if (higher > 0 || lower < 0) {
@@ -43,6 +48,7 @@ class MainPage extends JFrame {
                 break;
             }
         }
+
         // handle out of bounds data error OR ELSE show Main Screen
         if (!boundaryFlag) {
             JOptionPane.showMessageDialog(this,
@@ -50,17 +56,17 @@ class MainPage extends JFrame {
                     "Data Boundary Error",
                     JOptionPane.ERROR_MESSAGE);
         } else {
+            // Display Main Page
+            setVisible(true);
 
-            setVisible(true); // Display Main Page
-
-            // Add Section Label
+            // "Add" Section Label
             JLabel addSection = new JLabel("Add:");
             addSection.setFont(
-                    new Font("Serif",Font.BOLD,18));
+                    new Font("Serif", Font.BOLD, 18));
             addSection.setBounds(20, 20, 120, 30);
             add(addSection);
 
-            //append button
+            // Append button
             JButton appendFile = new JButton("Add a file");
             add(appendFile);
             appendFile.setBounds(20, 50, 120, 30);
@@ -92,8 +98,8 @@ class MainPage extends JFrame {
                                     "Data Boundary Error",
                                     JOptionPane.ERROR_MESSAGE);
                         } else {
-                            filedata.addAll(append); // Append to filedata
-                            System.out.println(filedata);
+                            fileData.addAll(append); // Append to fileData
+                            updateFlag = true;
                         }
                     } catch (Exception ex) {
                         ex.printStackTrace();
@@ -101,10 +107,18 @@ class MainPage extends JFrame {
                 }
             });
 
+            // Display TextArea
+            JTextArea displayData = new JTextArea();
+            displayData.setEditable(false);
+            displayData.setBounds(20, 150, 700, 450);
+            displayData.setLineWrap(true);
+            displayData.setVisible(false);
+            add(displayData);
+
             // Refresh Section Label
             JLabel refreshSection = new JLabel("Refresh:");
             refreshSection.setFont(
-                    new Font("Serif",Font.BOLD,18));
+                    new Font("Serif", Font.BOLD, 18));
             refreshSection.setBounds(20, 120, 200, 30);
             add(refreshSection);
 
@@ -113,10 +127,144 @@ class MainPage extends JFrame {
             display.setBounds(20, 150, 150, 30);
             add(display);
 
+            display.addActionListener(e -> {
+                displayData.setText("");
+                fileData.sort(null);
+                for (Integer element : fileData) {
+                    displayData.append(element + " ");
+                }
+                displayData.setVisible(true);
+            });
+
             // Display Graph button
             JButton displayGraph = new JButton("Display Graph");
             displayGraph.setBounds(20, 200, 150, 30);
             add(displayGraph);
+
+            // Calculate minimum and maximum value of dataset:
+            int max = Integer.MIN_VALUE;
+            int min = Integer.MAX_VALUE;
+
+            for (Integer i : fileData) {
+                if (max < i) {
+                    max = i;
+                }
+                if (min > i) {
+                    min = i;
+                }
+            }
+
+            int one = 0, two = 0, three = 0, four = 0, five = 0, six = 0,
+                    seven = 0, eight = 0, nine = 0, ten = 0;
+
+            for (Integer i : fileData) {
+                if (i <= (min + max) * 0.1)
+                    one += 1;
+                else if (i > (min + max) * 0.1 && i <= (min + max) * 0.2)
+                    two += 1;
+                else if (i > (min + max) * 0.2 && i <= (min + max) * 0.3)
+                    three += 1;
+                else if (i > (min + max) * 0.3 && i <= (min + max) * 0.4)
+                    four += 1;
+                else if (i > (min + max) * 0.4 && i <= (min + max) * 0.5)
+                    five += 1;
+                else if (i > (min + max) * 0.5 && i <= (min + max) * 0.6)
+                    six += 1;
+                else if (i > (min + max) * 0.6 && i <= (min + max) * 0.7)
+                    seven += 1;
+                else if (i > (min + max) * 0.7 && i <= (min + max) * 0.8)
+                    eight += 1;
+                else if (i > (min + max) * 0.8 && i <= (min + max) * 0.9)
+                    nine += 1;
+                else if (i > (min + max) * 0.9)
+                    ten += 1;
+            }
+
+            dataset.addValue(one, "0-10 %", "0-10 %");
+            dataset.addValue(two, "11-20 %", "11-20 %");
+            dataset.addValue(three, "21-30 %", "21-30 %");
+            dataset.addValue(four, "31-40 %", "31-40 %");
+            dataset.addValue(five, "41-50 %", "41-50 %");
+            dataset.addValue(six, "51-60 %", "51-60 %");
+            dataset.addValue(seven, "61-70 %", "61-70 %");
+            dataset.addValue(eight, "71-80 %", "71-80 %");
+            dataset.addValue(nine, "81-90 %", "81-90 %");
+            dataset.addValue(ten, "91-100 %", "91-100 %");
+
+
+            // Created Graph with current data
+            JFreeChart graph = ChartFactory.createBarChart(
+                    "Data by Percentages",
+                    "Percentage Range",
+                    "Count",
+                    dataset, PlotOrientation.HORIZONTAL,
+                    true, false, false);
+
+            // Setting the Y-axis to show only numbers instead of decimals
+            CategoryPlot chartPlot = graph.getCategoryPlot();
+            ValueAxis yAxis = chartPlot.getRangeAxis();
+            yAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+
+            ChartPanel chartPanel = new ChartPanel(graph);
+            add(chartPanel);
+
+            // Update Graph
+            displayGraph.addActionListener(e -> {
+                // Calculate minimum and maximum value of dataset:
+                int max1 = Integer.MIN_VALUE;
+                int min1 = Integer.MAX_VALUE;
+
+                for (Integer i : fileData) {
+                    if (max1 < i) {
+                        max1 = i;
+                    }
+                    if (min1 > i) {
+                        min1 = i;
+                    }
+                }
+
+                int first = 0, second = 0, third = 0, fourth = 0, fifth = 0,
+                        sixth = 0, seventh = 0, eighth = 0, ninth = 0, tenth = 0;
+
+                for (Integer i : fileData) {
+                    if (i <= (min1 + max1) * 0.1)
+                        first += 1;
+                    else if (i > (min1 + max1) * 0.1 && i <= (min1 + max1) * 0.2)
+                        second += 1;
+                    else if (i > (min1 + max1) * 0.2 && i <= (min1 + max1) * 0.3)
+                        third += 1;
+                    else if (i > (min1 + max1) * 0.3 && i <= (min1 + max1) * 0.4)
+                        fourth += 1;
+                    else if (i > (min1 + max1) * 0.4 && i <= (min1 + max1) * 0.5)
+                        fifth += 1;
+                    else if (i > (min1 + max1) * 0.5 && i <= (min1 + max1) * 0.6)
+                        sixth += 1;
+                    else if (i > (min1 + max1) * 0.6 && i <= (min1 + max1) * 0.7)
+                        seventh += 1;
+                    else if (i > (min1 + max1) * 0.7 && i <= (min1 + max1) * 0.8)
+                        eighth += 1;
+                    else if (i > (min1 + max1) * 0.8 && i <= (min1 + max1) * 0.9)
+                        ninth += 1;
+                    else if (i > (min1 + max1) * 0.9)
+                        tenth += 1;
+                }
+
+                DefaultCategoryDataset dataSet1 = new DefaultCategoryDataset();
+
+                dataSet1.addValue(first, "0-10 %", "0-10 %");
+                dataSet1.addValue(second, "11-20 %", "11-20 %");
+                dataSet1.addValue(third, "21-30 %", "21-30 %");
+                dataSet1.addValue(fourth, "31-40 %", "31-40 %");
+                dataSet1.addValue(fifth, "41-50 %", "41-50 %");
+                dataSet1.addValue(sixth, "51-60 %", "51-60 %");
+                dataSet1.addValue(seventh, "61-70 %", "61-70 %");
+                dataSet1.addValue(eighth, "71-80 %", "71-80 %");
+                dataSet1.addValue(ninth, "81-90 %", "81-90 %");
+                dataSet1.addValue(tenth, "91-100 %", "91-100 %");
+
+                // Set new dataset
+                chartPlot.setDataset(dataSet1);
+            });
 
             // Display Analysis button
             JButton displayAnalysis = new JButton("Data Analysis");
@@ -128,104 +276,18 @@ class MainPage extends JFrame {
             displayDistribution.setBounds(20, 300, 150, 30);
             add(displayDistribution);
 
-
-            // Display TextArea
-            JTextArea displayData = new JTextArea();
-            displayData.setEditable(false);
-            displayData.setBounds(20, 150, 700, 450);
-            displayData.setLineWrap(true);
-            displayData.setVisible(false);
-            add(displayData);
-            display.addActionListener(e -> {
-                displayData.setText("");
-                filedata.sort(null);
-                for (Integer element : filedata) {
-                    displayData.append(element + " ");
-                }
-                displayData.setVisible(true);
-            });
-
-            ChartPanel cp = createGraph(filedata);
-            add(cp);
-
             JTabbedPane tabbedPane = new JTabbedPane();
             tabbedPane.addTab("Display Data", displayData);
-            tabbedPane.addTab("Display Graph", cp);
+            tabbedPane.addTab("Display Graph", chartPanel);
             tabbedPane.addTab("Data Analysis", new JPanel());
             tabbedPane.addTab("Distribution Graph", new JPanel());
             tabbedPane.setBounds(200, 150, 1000, 450);
             add(tabbedPane);
             tabbedPane.setVisible(true);
 
-
         } // else Display main page
-
-
     }
 
-    private ChartPanel createGraph(List<Integer> data) {
-        // Calculate minimum and maximum value of dataset:
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-
-        for (Integer i : data) {
-            if (max < i) {
-                max = i;
-            }
-            if (min > i) {
-                min = i;
-            }
-        }
-
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        int one = 0, two = 0, three = 0, four = 0, five = 0, six = 0,
-                seven = 0, eight = 0, nine = 0, ten = 0;
-        for (Integer i : data) {
-            if (i <= (min + max) * 0.1)
-                one += 1;
-            else if (i > (min + max) * 0.1 && i <= (min + max) * 0.2)
-                two += 1;
-            else if (i > (min + max) * 0.2 && i <= (min + max) * 0.3)
-                three += 1;
-            else if (i > (min + max) * 0.3 && i <= (min + max) * 0.4)
-                four += 1;
-            else if (i > (min + max) * 0.4 && i <= (min + max) * 0.5)
-                five += 1;
-            else if (i > (min + max) * 0.5 && i <= (min + max) * 0.6)
-                six += 1;
-            else if (i > (min + max) * 0.6 && i <= (min + max) * 0.7)
-                seven += 1;
-            else if (i > (min + max) * 0.7 && i <= (min + max) * 0.8)
-                eight += 1;
-            else if (i > (min + max) * 0.8 && i <= (min + max) * 0.9)
-                nine += 1;
-            else if (i > (min + max) * 0.9)
-                ten += 1;
-        }
-
-        dataset.addValue(one, "0-10 %", "0-10 %");
-        dataset.addValue(two, "11-20 %", "11-20 %");
-        dataset.addValue(three, "21-30 %", "21-30 %");
-        dataset.addValue(four, "31-40 %", "31-40 %");
-        dataset.addValue(five, "41-50 %", "41-50 %");
-        dataset.addValue(six, "51-60 %", "51-60 %");
-        dataset.addValue(seven, "61-70 %", "61-70 %");
-        dataset.addValue(eight, "71-80 %", "71-80 %");
-        dataset.addValue(nine, "81-90 %", "81-90 %");
-        dataset.addValue(ten, "91-100 %", "91-100 %");
-
-        JFreeChart graph = ChartFactory.createBarChart(
-                "Data by Percentages",
-                "Percentage Range",
-                "Count",
-                dataset, PlotOrientation.HORIZONTAL,
-                true,false, false);
-
-        // Setting the Y-axis to show only numbers instead of decimals
-        CategoryPlot chartPlot = graph.getCategoryPlot();
-        ValueAxis yAxis = chartPlot.getRangeAxis();
-        yAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
-
-        return new ChartPanel(graph);
-    }
 }
+
+
